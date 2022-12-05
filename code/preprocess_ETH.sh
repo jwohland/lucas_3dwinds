e9b0face93004936b54f098db1ae111a48842f66
Jan Wohland <jan.wohland@hereon.de>
2022-12-05 15:26:59 +0100

    including loops over experiments and years

--- code/preprocess_ETH.sh
+++ code/preprocess_ETH.sh
@@ -1,35 +1,44 @@
 # Manual steps preceding this script
 # 1) Copy data from Edouard to cluster
 # 2) in data/ETH/XXXX/ rename COSMO_CLM2_EU-CORDEX_XXXX_191019/ to raw_data/ where XXXX is EVAL, FOREST or GRASS
-# 3) in raw_data directories, delete years prior to 1986 and delete empty directories temp_year where year in 1999 - 2010
+# 3) in raw_data directories, delete years prior to 1986 and delete empty directories temp_year where year in ${year} - 2010
 
 
-# todo loop over experiments
-# Unzip remaining years, only extracting relevant data
-# todo loop over years
-cd /net/xenon/climphys/jwohland/lucas_3dwinds/data/ETH/EVAL/
-tar -xvzf ./raw_data/1999.tar ./1999/output/6hr/
-tar -xvzf ./raw_data/1999.tar ./1999/output/3D/
-mv 1999/output/* 1999
-rm -r 1999/output
-
-#combine all timesteps in a year
-cdo copy 1999/6hr/*.nc 1999/6hr_1999.nc
-cdo copy 1999/3D/*.nc 1999/3D_1999.nc
-
-# create directory structure with one variable per folder
-mkdir -p U_10M V_10M FI T U V RELHUM
-
-#choose variables from 6hr and store in separate files
-cdo selvar,U_10M 1999/6hr_1999.nc U10M/U10M_1999.nc
-cdo selvar,V_10M 1999/6hr_1999.nc V10M/V10M_1999.nc
-
-#choose variables from 3D and store in separate files
-cdo sellevel,92500,85000 -selvar,FI 1999/3D_1999.nc FI/FI_1999.nc
-cdo sellevel,92500,85000 -selvar,T 1999/3D_1999.nc T/T_1999.nc
-cdo sellevel,92500,85000 -selvar,U 1999/3D_1999.nc U/U_1999.nc
-cdo sellevel,92500,85000 -selvar,V 1999/3D_1999.nc V/V_1999.nc
-cdo sellevel,92500,85000 -selvar,RELHUM 1999/3D_1999.nc RELHUM/RELHUM_1999.nc
-
-#cleanup
-rm -r 1999
+# loop over experiments
+for experiment in EVAL FOREST GRASS
+do
+  echo $experiment
+  # Unzip remaining years, only extracting relevant data
+  # loop over years
+  for year in {1986..2015}
+  do
+    echo $year
+  
+    cd /net/xenon/climphys/jwohland/lucas_3dwinds/data/ETH/${experiment}/
+    tar -xzf ./raw_data/${year}.tar ./${year}/output/6hr/
+    tar -xzf ./raw_data/${year}.tar ./${year}/output/3D/
+    mv ${year}/output/* ${year}
+    rm -r ${year}/output
+    
+    #combine all timesteps in a year
+    cdo copy ${year}/6hr/*.nc ${year}/6hr_${year}.nc
+    cdo copy ${year}/3D/*.nc ${year}/3D_${year}.nc
+    
+    # create directory structure with one variable per folder
+    mkdir -p U_10M V_10M FI T U V RELHUM
+    
+    #choose variables from 6hr and store in separate files
+    cdo selvar,U_10M ${year}/6hr_${year}.nc U10M/U10M_${year}.nc
+    cdo selvar,V_10M ${year}/6hr_${year}.nc V10M/V10M_${year}.nc
+    
+    #choose variables from 3D and store in separate files
+    cdo sellevel,92500,85000 -selvar,FI ${year}/3D_${year}.nc FI/FI_${year}.nc
+    cdo sellevel,92500,85000 -selvar,T ${year}/3D_${year}.nc T/T_${year}.nc
+    cdo sellevel,92500,85000 -selvar,U ${year}/3D_${year}.nc U/U_${year}.nc
+    cdo sellevel,92500,85000 -selvar,V ${year}/3D_${year}.nc V/V_${year}.nc
+    cdo sellevel,92500,85000 -selvar,RELHUM ${year}/3D_${year}.nc RELHUM/RELHUM_${year}.nc
+    
+    #cleanup
+    rm -r ${year}
+  done
+done
