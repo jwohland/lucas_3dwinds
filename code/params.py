dee4e86a10c48a8a7fc0d607b48513d2a8efdbb0
Jan Wohland <jan.wohland@hereon.de>
2022-12-12 10:48:23 +0100

    Adding limits for horizontal grid

---
+++ code/params.py
@@ -0,0 +1,10 @@
+
+# Spatial extent of IDL grid. Most other grids have additional area outside this box which
+# should be removed as only available in subset of models.
+# Values are taken from computation in notebooks/5_horizontal_grids.ipynb
+horizontal_ranges = {
+    "rlon_max": 17.99,
+    "rlon_min": -28.21,
+    "rlat_max": 21.67,
+    "rlat_min": -23.21,
+}
