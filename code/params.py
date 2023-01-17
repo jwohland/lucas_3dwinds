9e59bf1224e11bff7f6a0c84046c82b854ede8d0
jwohland <jan.wohland@env.ethz.ch>
2023-01-17 17:02:44 +0100

    ranges in different format

--- code/params.py
+++ code/params.py
@@ -2,10 +2,8 @@
 # should be removed as only available in subset of models.
 # Values are taken from computation in notebooks/5_horizontal_grids.ipynb
 horizontal_ranges = {
-    "rlon_max": 17.99,
-    "rlon_min": -28.21,
-    "rlat_max": 21.67,
-    "rlat_min": -23.21,
+    "rlons": slice(-28.21, 17.99),
+    "rlats": slice(-23.21, 21.67)
 }
 
 EXPERIMENTS = ["GRASS", "FOREST", "EVAL"]
