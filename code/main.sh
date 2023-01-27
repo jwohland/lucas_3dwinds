3977afe7a5433dcbcd743f40419795cd77fdf735
jwohland <jan.wohland@env.ethz.ch>
2023-01-27 10:21:55 +0100

    First part of JLU preprocessing and addition of main script that glues everything together

---
+++ code/main.sh
@@ -0,0 +1,21 @@
+# Overview and sequencing of scripts
+
+
+# Preprocessing per model
+bash preprocess_ETH.sh
+python preprocess_ETH.py
+
+bash preprocess_GERICS.sh
+python preprocess_GERICS.py
+
+python preprocess_OUR.py
+
+bash preprocess_IDL.sh
+python preprocess_IDL.py
+
+bash preprocess_JLU.sh
+
+# Compute monthly aggregates
+python compute_monthly_winds.py
+
+# Analysis
---
+++ code/preprocess_JLU.sh
@@ -0,0 +1,36 @@
+# Create directory structure and move raw_data into correct directories
+cd ../data/JLU
+
+
+
+for experiment in EVAL FOREST GRASS
+do
+  for variable in U V U_10M V_10M FI
+  do
+    # Create directory structure
+    mkdir -p ${experiment}/${variable}
+  done
+done
+
+# EVAL has different directory structure than GRASS and FOREST
+# EVAL: variable_name/variable_name*.nc
+# FOREST & GRASS: variable_name.nc
+# Move all variables out of extra folders for EVAL
+mv EVAL/raw_data/*/* EVAL/raw_data/
+
+# Copy data
+for experiment in EVAL FOREST GRASS
+do
+  # doing this explicitly per variable here because wildcards a bit tricky, e.g.,
+  # U_10 and U850 both show up using U*
+  echo ${experiment}
+  for wind_direction in U V
+  do
+    cp ${experiment}/raw_data/${wind_direction}_10M* ${experiment}/${wind_direction}_10M
+    for pressure_level in 925 850 750  # this trows some errors because 925 only in FOREST and GRASS and 750 only in EVAL
+    do
+      cp ${experiment}/raw_data/${wind_direction}${pressure_level}* ${experiment}/${wind_direction}
+    done
+  done
+  cp ${experiment}/raw_data/FI* ${experiment}/FI
+done
