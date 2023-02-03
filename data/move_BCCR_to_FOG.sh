f52fa752ee98ca968cffaddc946c4875a3d7f4cb
jwohland <jan.wohland@env.ethz.ch>
2023-02-03 15:25:37 +0100

    bug fix

--- data/move_BCCR_to_FOG.sh
+++ data/move_BCCR_to_FOG.sh
@@ -11,7 +11,7 @@ do
     # retrieve data
     scp jwohland@juelich:/home/jwohland/fpslucas/PHASE1/${experiment}/EUR-44/BCCR/ERAINT/evaluation/BCCR-WRFV381/v1/r1i1p1/1hr/${variable}*.tar $target_dir
     # extract data
-    for filename in ls ${target_dir}/*.tar
+    for filename in ${target_dir}/*.tar
     do
       tar -xf ${filename} -C ${target_dir}
     done
