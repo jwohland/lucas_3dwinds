bfd5450150c8324a783acd8284ba993a5dcc3f8a
jwohland <jan.wohland@env.ethz.ch>
2023-02-03 15:05:29 +0100

    Unzipping had to do per file. Wildcard directly in the tar command did not work.

--- data/move_BCCR_to_FOG.sh
+++ data/move_BCCR_to_FOG.sh
@@ -11,7 +11,10 @@ do
     # retrieve data
     scp jwohland@juelich:/home/jwohland/fpslucas/PHASE1/${experiment}/EUR-44/BCCR/ERAINT/evaluation/BCCR-WRFV381/v1/r1i1p1/1hr/${variable}*.tar $target_dir
     # extract data
-    tar -xvf ${target_dir}/*.tar
+    for filename in ls ${target_dir}/*.tar
+    do
+      tar -xf ${filename} -C ${target_dir}
+    done
     rm ${target_dir}/*.tar
   done
 done
