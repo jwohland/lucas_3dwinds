a65a75e109f0d467380a0f3672dc3e517d1dd871
jwohland <jan.wohland@env.ethz.ch>
2023-02-09 13:49:30 +0100

    Renamed notebook reference

--- code/params.py
+++ code/params.py
@@ -1,6 +1,6 @@
 # Spatial extent of IDL grid. Most other grids have additional area outside this box which
 # should be removed as only available in subset of models.
-# Values are taken from computation in notebooks/5_horizontal_grids.ipynb
+# Values are taken from computation in notebooks/05_horizontal_grids.ipynb
 horizontal_ranges = {
     "rlons": slice(-28.21, 17.99),
     "rlats": slice(-23.21, 21.67)
