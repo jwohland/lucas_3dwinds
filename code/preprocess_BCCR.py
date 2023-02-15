63250d192616ae3704227bcd19e049afa5d9f5e4
jwohland <jan.wohland@env.ethz.ch>
2023-02-15 10:26:59 +0100

    Bug fix for 1986 EVAL v component (has wrong horizontal grid names)

--- code/preprocess_BCCR.py
+++ code/preprocess_BCCR.py
@@ -41,6 +41,20 @@ for experiment in ["EVAL", "FOREST", "GRASS"]:
             ).rename({var: "V"})
             for var in var_names
         ]
+        # Fix bug in 1986 EVAL meta data. See issue #27 for explanation
+        if ((experiment == "EVAL") & (year == 1986)):
+            ds_problem = ds_list[0]
+            ds_problem = ds_problem.rename({"x": "rlon", "y": "rlat"})
+            ds_correct = xr.open_dataset(
+                data_path
+                + "/EVAL/raw_data/va1000_EUR44_ECMWF-ERAINT_EVAL_r1i1p1_BCCR-WRF381_1hr_1987010100-1987123123.nc"
+            )  # file with correct meta data
+            for coordinate in ["rlat", "rlon", "plev"]:
+                ds_problem[coordinate] = ds_correct.coords[coordinate].values
+            for variable in ["rotated_pole", "lat", "lon"]:
+                ds_problem[variable] = ds_correct[variable]
+            ds_list[0] = ds_problem
+
         ds_va = xr.merge(ds_list)
         ds_va.to_netcdf(data_path_tmp + "V/V_" + str(year) + ".nc")
 
@@ -74,7 +88,9 @@ for experiment in ["EVAL", "FOREST", "GRASS"]:
         #####################
 
         folder_dictionary = {"100m": "100", "s": "10"}
-        var_name_endings = ["s"] # ["100m", "s"]  # 100m winds essentially not usable because FOREST 100m v component does not exist. See issue #25
+        var_name_endings = [
+            "s"
+        ]  # ["100m", "s"]  # 100m winds essentially not usable because FOREST 100m v component does not exist. See issue #25
 
         for var_name_ending in var_name_endings:
             ds_ua = xr.open_mfdataset(
