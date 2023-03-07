e3f84be634b68578a12d5d9f2233c39b234aa4d5
Jan Wohland <jan.wohland@env.ethz.ch>
2023-03-07 16:26:42 +0100

    Make approximate_height.yaml import possible from within a notebook.

--- code/params.py
+++ code/params.py
@@ -2,12 +2,14 @@
 # should be removed as only available in subset of models.
 # Values are taken from computation in notebooks/05_horizontal_grids.ipynb
 import yaml
+import sys
+sys.path.append(".")
 
 horizontal_ranges = {"rlons": slice(-28.21, 17.99), "rlats": slice(-23.21, 21.67)}
 
 EXPERIMENTS = ["GRASS", "FOREST", "EVAL"]
 
 # simple approximate mapping from model levels and/or pressure levels to meters above ground
-with open("approximate_heights.yaml", mode="r") as file:
+with open("../code/approximate_heights.yaml", mode="r") as file:
     approximate_heights = yaml.safe_load(file)
 
