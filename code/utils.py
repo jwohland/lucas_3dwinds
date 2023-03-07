c496933d368be775ce6d81d46113f17111b0d7ae
jwohland <jan.wohland@env.ethz.ch>
2023-03-07 16:14:48 +0100

    Expanding utils

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 import cartopy.crs as ccrs
 import cartopy.feature as cf
+import numpy as np
 
 SUBPLOT_KW = {
     "subplot_kw": {"projection": ccrs.PlateCarree(), "extent": [-15, 50, 35, 70]}
@@ -16,6 +17,36 @@ TEXT_PARAMS = {
     "verticalalignment": "center",
 }
 
+
 def add_coast_boarders(ax):
     ax.add_feature(cf.COASTLINE)
     ax.add_feature(cf.BORDERS)
+
+
+vertical_dim_dic = {
+    "IDL": "mlev",
+    "ETH": "pressure",
+    "GERICS": "lev",
+    "OUR": "plev",
+    "BCCR": "plev",
+}
+
+
+def constrain_vertical_range(ds, institution):
+    """
+    Restrict 3D data to height levels that are of interest for this analysis.
+        GERICS: 22 - 27
+        IDL: 0-5
+    And sort entries in decreasing order. Initially:
+        from high to low: GERICS, BCCR, ETH
+        from low to high: OUR, IDL
+    BCCR needs no specific handling
+    """
+    if institution in ["GERICS", "IDL"]:
+        ranges = {"GERICS": np.arange(22, 28), "IDL": np.arange(6)}
+        dim_name = vertical_dim_dic[institution]
+        ds = ds.sel({dim_name: ranges[institution]})
+    if institution in ["OUR", "IDL"]:
+        vertical_dim = vertical_dim_dic[institution]
+        ds = ds.reindex(vertical_dim=list(reversed(ds[vertical_dim])))
+    return ds
