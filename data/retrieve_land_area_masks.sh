829ff353cc30b1edf167d6ac2db9e394f55b7891
jwohland <jan.wohland@env.ethz.ch>
2023-03-08 15:41:55 +0100

    Retrieval of land-sea-lakes masks

---
+++ data/retrieve_land_area_masks.sh
@@ -0,0 +1,8 @@
+# Execute on fog
+fog
+
+# GERICS is unclear
+scp juelich:~/fpslucas/PHASE1/FOREST/EUR-44/BCCR/ERAINT/evaluation/BCCR-WRFV381/v1/r1i1p1/fx/sflt_EUR44_ECMWF-ERAINT_FOREST_r1i1p1_BCCR-WRF381_fx.nc ../data/BCCR
+scp juelich:~/fpslucas/PHASE1/FOREST/EUR-44/OUR/ERAINT/evaluation/OUR-CRCM5/v1/r1i1p1/static/orogf/orogf_cfe_197900_se.nc ../data/OUR
+scp juelich:~/fpslucas/PHASE1/EVAL/EUR-44/ETH/ERAINT/evaluation/CCLM_CLM5.0/v1/r1i1p1/fx/sftlf/sftlf_EUR-44_ERAint_COSMO_CLM2_EU-CORDEX_EVAL_191019_r0i0p0_CLMcom-ETH-CCLM-CLM5.0_x0n1v1_fx.nc ../data/ETH
+scp juelich:~/fpslucas/PHASE1/EVAL/EUR-44/IDL/ERAINT/evaluation/IDL-WRFV381D/v1/r1i1p1/fx/sftlf/sflt_EUR-44_ECMWF-ERAINT_LUCAS_EVAL_r1i1p1_IDL_WRFV381D_v1_fx.nc ../data/IDL
