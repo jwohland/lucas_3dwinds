a26c3ec7c76579c891c064cd24695b3b6203836d
jwohland <jan.wohland@env.ethz.ch>
2023-07-18 10:58:51 +0200

    Geopotential retrievals for IDL

--- data/move_IDL_to_levante.sh
+++ data/move_IDL_to_levante.sh
@@ -11,14 +11,3 @@ scp -pr /home/jwohland/fpslucas/PHASE1/GRASS/EUR-44/IDL/ERAINT/evaluation/*/*/*/
 # FOREST
 scp -pr /home/jwohland/fpslucas/PHASE1/FOREST/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/ua g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/FOREST
 scp -pr /home/jwohland/fpslucas/PHASE1/FOREST/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/va g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/FOREST
-
-# copy IDL geopotential height
-# EVAL
-scp -pr /home/jwohland/fpslucas/PHASE1/EVAL/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/EVAL
-scp -pr /home/jwohland/fpslucas/PHASE1/EVAL/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/EVAL
-# GRASS
-scp -pr /home/jwohland/fpslucas/PHASE1/GRASS/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/GRASS
-scp -pr /home/jwohland/fpslucas/PHASE1/GRASS/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/GRASS
-# FOREST
-scp -pr /home/jwohland/fpslucas/PHASE1/FOREST/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/FOREST
-scp -pr /home/jwohland/fpslucas/PHASE1/FOREST/EUR-44/IDL/ERAINT/evaluation/*/*/*/1hr/zg g300106@levante.dkrz.de:/work/ch0636/g300106/projects/lucas_3dwinds/data/IDL/FOREST
