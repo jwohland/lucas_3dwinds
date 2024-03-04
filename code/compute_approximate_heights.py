f9e312bff0571ed21fe24dc2c89a5253ccf1bbf2
jwohland <jan.wohland@env.ethz.ch>
2024-03-04 12:13:44 +0100

    Formatting and limited analysis to the 2 models that are actually used

--- code/compute_approximate_heights.py
+++ code/compute_approximate_heights.py
@@ -11,7 +11,7 @@ plot_path = "../plots/vertical_coordinate/"
 
 
 def plot_mean_std_heights(ds_height, zdim_name, zdim_range, ins, nrows=6):
-    f, axs = plt.subplots(ncols=3, nrows=nrows, figsize=(12, 16/6*nrows))
+    f, axs = plt.subplots(ncols=3, nrows=nrows, figsize=(12, 16 / 6 * nrows))
     for i, lev_2 in enumerate(zdim_range):
         ds_tmp = ds_height.sel({zdim_name: lev_2})
         ds_tmp.mean(dim="time").plot(ax=axs[i, 0])
@@ -52,20 +52,17 @@ def plot_mean_std_heights(ds_height, zdim_name, zdim_range, ins, nrows=6):
 
 
 # GERICS
-ds_GERICS_FI = xr.open_dataset(
-    "../data/GERICS/GRASS/FI_interpolated/FI_2000.nc"
-)  # todo this is only grass
+ds_GERICS_FI = xr.open_dataset("../data/GERICS/GRASS/FI_interpolated/FI_2000.nc")
 ds_GERICS_FIB = xr.open_dataset("../data/GERICS/GRASS/FIB/FIB_2000.nc")
 ds_GERICS_height = ds_GERICS_FI["FI"] - ds_GERICS_FIB["FIB"]
 
 
 plot_mean_std_heights(ds_GERICS_height, "lev", range(22, 28), "GERICS")
-# todo output mean level and standard deviations to a dictionary or so
 
 # IDL
 ds_IDL_zg = xr.open_dataset(
     "../data/IDL/EVAL/zg/zg_EUR-44_ECMWF-ERAINT_LUCAS_EVAL_r1i1p1_IDL_WRFV381D_v1_1hr_2000010100-2000123123.nc"
-)  # todo this is only EVAL
+)
 ds_IDL_oro = xr.open_dataset(
     "../data/IDL/orog_EUR-44_ECMWF-ERAINT_LUCAS_EVAL_r1i1p1_IDL_WRFV381D_v1_fx.nc"
 )
@@ -75,39 +72,3 @@ for ds_tmp in [ds_IDL_zg, ds_IDL_oro]:
         ds_tmp[rdim] = np.round(ds_tmp[rdim], 2)
 ds_IDL_height = ds_IDL_zg["zg"] - ds_IDL_oro["orog"].drop(["lon", "lat"])
 plot_mean_std_heights(ds_IDL_height, "mlev", range(6), "IDL")
-
-# BCCR
-ds_zg = xr.open_dataset("../data/BCCR/GRASS/ZG/ZG_2000.nc").drop("rotated_pole")
-ds_oro = xr.open_dataset(
-    "../data/IDL/orog_EUR-44_ECMWF-ERAINT_LUCAS_EVAL_r1i1p1_IDL_WRFV381D_v1_fx.nc"
-).drop("rotated_pole")
-# I.e., assuming all use the same orography
-for ds_tmp in [ds_zg, ds_oro]:
-    for rdim in ["rlat", "rlon"]:
-        ds_tmp[rdim] = np.round(ds_tmp[rdim], 2)
-ds_height = ds_zg["zg"] - ds_oro["orog"]
-plot_mean_std_heights(
-    ds_height.drop(["lat", "lon"]),
-    "plev",
-    np.array([70000.0, 85000.0, 92500.0, 100000.0]),
-    "BCCR",
-    nrows=4,
-)
-
-# ETH todo this does not work well yet. Oro grid appears different. Only few points overlap!!
-ds_zg = xr.open_dataset("../data/ETH/GRASS/FI/FI_2000.nc").drop("rotated_pole")
-ds_oro = xr.open_dataset(
-    "../data/ETH/orog_EUR-44_ERAint_COSMO_CLM2_EU-CORDEX_EVAL_191019_r0i0p0_CLMcom-ETH-CCLM-CLM5.0_x0n1v1_fx.nc"
-).drop("rotated_pole")
-# I.e., assuming all use the same orography
-for ds_tmp in [ds_zg, ds_oro]:
-    for rdim in ["rlat", "rlon"]:
-        ds_tmp[rdim] = np.round(ds_tmp[rdim], 1)
-ds_height = ds_zg["FI"] - ds_oro["orog"]
-plot_mean_std_heights(
-    ds_height.drop(["lat", "lon"]),
-    "pressure",
-    np.array([85000., 92500.]),
-    "ETH",
-    nrows=2,
-)
