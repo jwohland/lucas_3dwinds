35c88bd1bb5ef2c7bb0fba2ff1f786e6651328fe
jwohland <jan.wohland@env.ethz.ch>
2024-03-04 12:09:48 +0100

    Main script is restructured (prep, compute, analyse) and some outdated scripts are not executed any more

--- code/main.sh
+++ code/main.sh
@@ -1,35 +1,26 @@
 # Overview and sequencing of scripts
 
-
-# Preprocessing per model
-bash preprocess_ETH.sh
-python preprocess_ETH.py
-
+###############
+# Preprocessing
+###############
 bash preprocess_GERICS.sh
 python preprocess_GERICS.py
-
-python preprocess_OUR.py
-
 bash preprocess_IDL.sh
 python preprocess_IDL.py
 
-bash preprocess_JLU.sh
+###############
+# Computation
+###############
+python compute_approximate_heights.py  # Approximate  heights per model level
+python compute_monthly_winds.py  # monthly aggregates
+python compute_sub-daily_focusareas.py  # sub-daily in focus areas
+python compute_hub_height.py  # hub height winds
+python compute_power.py  # power generation
 
-python preprocess_BCCR.py
-
-# Compute monthly aggregates
-python compute_monthly_winds.py
-python compute_sub-daily_focusareas.py
-
-# Analysis
-python analyze_monthly_means.py
-python analyze_subdaily.py
-python analyze_summernights_IDL.py
-
-# Compute hub height winds
-python compute_hub_height.py
-# Compute power generation
-python compute_power.py
-
-# Evaluation power generation
-python analyze_generation.py
+###############
+# Analysis / Plotting
+###############
+python analyze_monthly_means.py  # Mean wind changes
+python analyze_subdaily.py  # Daily cycle
+python analyze_summernights_IDL.py  # Summernights / Jet in IDL
+python analyze_generation.py  # power generation
