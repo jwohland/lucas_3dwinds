73593de42457b521a4f9ebb6d5c67c8643955c5b
jwohland <jan.wohland@env.ethz.ch>
2024-03-04 12:14:39 +0100

    Limiting to the 2 models that are actually used

--- code/compute_monthly_winds.py
+++ code/compute_monthly_winds.py
@@ -6,22 +6,18 @@ data_dir = "../data/"
 target_dir = "../data/monthly/"
 
 for experiment in ["FOREST", "GRASS"]:
-    for institution in ["NORCE", "GERICS", "ETH", "IDL", "OUR"]:  # todo include JLU once preprocessed
+    for institution in ["GERICS", "IDL"]:
         print(institution)
         ds = xr.open_mfdataset(data_dir + institution + "/" + experiment + "/S/*.nc")
         if institution == "GERICS":
             # time index needs modification to be understood by xarray
-            ds["time"] = pd.date_range(start="19860101.", periods=ds.time.size, freq="6h")
+            ds["time"] = pd.date_range(
+                start="19860101.", periods=ds.time.size, freq="6h"
+            )
         # Resample to monthly values
         ds = ds.resample(time="1MS").mean(dim="time").compute()
         # use same lat-lon coordinates
-        ds = ds.sel({"rlat": horizontal_ranges["rlats"], "rlon": horizontal_ranges["rlons"]})
+        ds = ds.sel(
+            {"rlat": horizontal_ranges["rlats"], "rlon": horizontal_ranges["rlons"]}
+        )
         ds.to_netcdf(target_dir + experiment + "/S_" + institution + ".nc")
-
-    # repeat for S_10 ETH  # todo check if 10m wind components should also be computed for other models
-    ds = xr.open_mfdataset(data_dir + "ETH/" + experiment + "/S_10M/*.nc")
-    # Resample to monthly values
-    ds = ds.resample(time="1MS").mean(dim="time").compute()
-    # use same lat-lon coordinates
-    ds = ds.sel({"rlat": horizontal_ranges["rlats"], "rlon": horizontal_ranges["rlons"]})
-    ds.to_netcdf(target_dir + experiment + "/S10M_ETH.nc")
